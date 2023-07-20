f6c450376731b8f0402ea465974c15585845e5ea
m-vishnuraj <mvishnurajm@gmail.com>
2023-07-20 07:59:48 +0530

    var let const completed

---
+++ script.js
@@ -0,0 +1,22 @@
+// var let const
+
+// ! var
+// * re declaration and reassign is possible
+// var name;
+
+// name = "Vishnu";
+
+// var name = "Ram";
+// console.log(name);
+
+// ! let
+// * re declaration not possible and reassign is possible
+
+// let name;
+// name = "Vishnu";
+// name = "Ram";
+// console.log(name);
+
+// ! const
+// * re declaration and reassign is not possible
+const name = "Vishnu";
