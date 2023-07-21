8d65726dc261391f8177e569455551bd42f8b7af
m-vishnuraj <mvishnurajm@gmail.com>
2023-07-21 09:20:00 +0530

    map and filter completed

--- script.js
+++ script.js
@@ -34,6 +34,28 @@ const name = "Vishnu";
 // }
 
 // ! TERNARY OPERATOR
-const age = 18;
+// const age = 18;
 
-age > 17 ? alert("You are eligible") : alert("You are not eligible");
+// age > 17 ? alert("You are eligible") : alert("You are not eligible");
+
+// ! MAP METHOD
+
+const numbers = [1, 2, 3, 4, 5];
+
+const double = numbers.map((numbers) => {
+  return numbers * 2;
+});
+
+console.log(double);
+
+// ! FILTER METHOD
+
+const ages = [18, 19, 20, 21, 22, 23, 24, 25];
+
+// above 20
+
+const above20 = ages.filter((age) => {
+  return age >= 20;
+});
+
+console.log(above20);
